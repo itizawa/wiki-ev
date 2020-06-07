2a2655b4e9baaad2d80f3d3e33906e862cfbe504
itizawa <itizawa@weseek.co.jp>
2020-06-07 19:48:35 +0900

    add alias

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const path = require("path");
 module.exports = {
   webpack(config, options) {
     config.resolve.alias["@components"] = path.join(__dirname, "components");
+    config.resolve.alias["@containers"] = path.join(__dirname, "containers");
     config.resolve.alias["@middlewares"] = path.join(
       __dirname,
       "lib/middlewares"
