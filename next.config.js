313465d827d33adf21487011cc615a8a86d95ad0
itizawa <itizawa@weseek.co.jp>
2020-06-14 01:13:35 +0900

    format

--- next.config.js
+++ next.config.js
@@ -6,14 +6,8 @@ module.exports = {
   webpack(config, options) {
     config.resolve.alias["@components"] = path.join(__dirname, "components");
     config.resolve.alias["@containers"] = path.join(__dirname, "containers");
-    config.resolve.alias["@middlewares"] = path.join(
-      __dirname,
-      "lib/middlewares"
-    );
-    config.resolve.alias["@widgets"] = path.join(
-      __dirname,
-      "components/widgets"
-    );
+    config.resolve.alias["@middlewares"] = path.join(__dirname, "lib/middlewares");
+    config.resolve.alias["@widgets"] = path.join(__dirname, "components/widgets");
     config.resolve.alias["@atoms"] = path.join(__dirname, "atoms");
     config.resolve.alias["@utils"] = path.join(__dirname, "lib/utils");
     config.resolve.alias["@lib"] = path.join(__dirname, "lib");
